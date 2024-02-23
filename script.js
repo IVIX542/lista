9d046071ff8b11e76b2916c4934431805b765c5a
IVIX542 <87498315+IVIX542@users.noreply.github.com>
2024-02-23 10:45:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var opcion = document.getElementById("lista").value;
+function buscar(){
+    var opcion = document.getElementById("lista").value;
+
+    switch(opcion){
+        case "juan":
+            document.getElementById("m").checked = true;
+            document.getElementById("madrid").checked = true;
+            break;
+        case "sofia":
+            document.getElementById("f").checked = true;
+            document.getElementById("18").checked = true;
+            document.getElementById("madrid").checked = false;
+            break;
+        case "mario":
+            document.getElementById("m").checked = true;
+            document.getElementById("18").checked = true;
+            document.getElementById("madrid").checked = true;
+            break;
+        case "maria":
+            document.getElementById("18").checked = false;
+            document.getElementById("f").checked = true;
+            document.getElementById("madrid").checked = true;
+            break;
+        
+    }
+}
+
